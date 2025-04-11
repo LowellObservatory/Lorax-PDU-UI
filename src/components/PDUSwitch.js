de2edf417d08d9671a12ef1a144ebe512cb73b64
Dyer Lytle <dlytle@lowell.edu>
2025-04-11 08:17:22 -0700

    disable NUC switch

--- src/components/PDUSwitch.js
+++ src/components/PDUSwitch.js
@@ -9,26 +9,38 @@ function PDUSwitch(props) {
 
     const [switchState, setSwitchState] = useState(props.state);
     const [drawerState, setDrawerState] = useState(false);
+    const [disabled, setDisabled] = useState(true);
    
     // If the state of the switch (in props) changes set the switchState
     // to that value.
     useEffect(() => {
         setSwitchState( props.state )
         setDrawerState(!drawerState);
-        
-    }, [ props.state ])  
+    }, [ props.state ]);
+
+    // If this is the "NUC", disable the switch
+    useEffect(() => {
+        if (props.switchLabel != "NUC") {
+            setDisabled(false);
+        }
+    }, []);
+
 
     // The callback for when the user clicks on a switch.
     const handleChange=(e)=>{
-        setSwitchState(!switchState);
-        var num = props.switchnum;
-        props.sendSwitch(props.control_topic, num, switchState);
+        console.log(props.switchLabel);
+        if (props.switchLabel != "NUC") {
+            setSwitchState(!switchState);
+            var num = props.switchnum;
+            props.sendSwitch(props.control_topic, num, switchState);
+        }
     }
 
     return (
         // The switch has a label that comes in from props and is
         // set to the value in 'switchState', true or false.
         <Form.Check
+            disabled={disabled}
             style={props.pending ? { opacity:0.5, color: "red" } : { color: "black" }}
             type="switch"
             id="outlet-switch"
